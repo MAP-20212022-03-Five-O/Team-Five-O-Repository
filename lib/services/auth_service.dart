01e7fd7fabe2135f299d0ab1dc13deab80fda06b
Umi Suhana <umisuhanarahman01@gmail.com>
2022-05-17 00:17:13 +0800

    Merge conflict fixed

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,27 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'database_manager.dart';
 
 class authService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //registration
-  Future createAccount(String name, String ic, String phoneno, String email,
-      String password) async {
+  //login
+  Future login(String _email, String _password) async {
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      User? user = result.user;
-      await DatabaseManager().storeUserDetails(
-        ic,
-        name,
-        phoneno,
-        email,
-        password,
-        user!.uid,
-      );
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      User? user = userCredential.user;
       return user;
+    } on FirebaseAuthException catch (e) {
+      print("Error: $e");
     } catch (e) {
-      print(e.toString());
+      print("Error: $e");
     }
   }
 }
